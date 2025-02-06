b9425418ac8077867a11f3de53f3bebdfbe99473
jimin <nijo5093@gmail.com>
2025-02-06 16:13:49 +0900

    koreaexim5

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-"# koreaexim-api" 
+
+
+# Koreaexim API Status
+
+이 리포지토리는 Koreaexim API를 사용하여 환율 정보를 자동으로 업데이트합니다.
+
+## 현재 환율
+> ['| None | None | None |']
+
+⏳ 업데이트 시간: 2025-02-06 16:13:34 (UTC)
+
+---
+자동 업데이트 봇에 의해 관리됩니다.
--- update_koreaexim.py
+++ update_koreaexim.py
@@ -6,46 +6,40 @@ from datetime import datetime
 load_dotenv()
 today = datetime.today().strftime("%Y%m%d")
 API_KEY = os.getenv("AUTHKEY")
-URL = f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={API_KEY}&searchdate={today}&data=AP01"
+URL = f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?{API_KEY}&searchdate={today}&data=AP01"
 
 
 # README 파일 경로
 README_PATH = "README.md"
 
-def get_koreaexim():
+def get_weather():
     """koreaexim API를 호출하여 서울의 날씨 데이터를 가져옴"""
     response = requests.get(URL)
     if response.status_code == 200:
         data = response.json()
-        ttb = data["ttb"]
-        cur_nm = data["cur_nm"]
-        tts = data["tts"]
-        return f"국가/통화명	: {cur_nm}, 전신환(송금)받으실때: {ttb}°C, 전신환(송금)보내실때: {tts}%"
+        currency = []
+        for cur in data:
+            cur_nm = cur["cur_nm"]
+            ttb = cur["ttb"]
+            tts = cur["tts"]
+            currency.append(f"| {cur_nm} | {ttb} | {tts} |")
+        return currency
     else:
-        return "환율 정보를 가져오는 데 실패했습니다."
-
+        return []
+    
 def update_readme():
     """README.md 파일을 업데이트"""
-    koreaexim_info = get_koreaexim()
+    weather_info = get_weather()
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    if not koreaexim_info:
-        currency_table = "환율 정보를 가져오는 데 실패했습니다."
-    else:
-        # 마크다운 표 헤더와 구분선 추가
-        currency_table = """| 국가/통화명 | 전신환(송금)받으실때 | 전신환(송금)보내실때 |
-|------------|------------------|------------------|
-"""
-        currency_table += "\n".join(koreaexim_info)
 
     readme_content = f"""
 
-#Koreaexim API Status
+# Koreaexim API Status
 
-이 리포지토리는 한국수출입은행 API를 사용하여 환율 정보를 자동으로 업데이트합니다.
+이 리포지토리는 Koreaexim API를 사용하여 환율 정보를 자동으로 업데이트합니다.
 
 ## 현재 환율
-> {koreaexim_info}
+> {weather_info}
 
 ⏳ 업데이트 시간: {now} (UTC)
 
